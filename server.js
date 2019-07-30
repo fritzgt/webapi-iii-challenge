4b6cfb8e249a449e1c274ffb8032920c970f4ed7
fritzgt <tuanis@live.com>
2019-07-30 07:49:54 -0400

    completed get and delete

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ server.use('/users', userRouter);
 
 //importing CRUD module for post
 const postRouter = require('./posts/postRouter');
-server.use('/users/:id/post', postRouter);
+// server.use('/users/:id/post', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
--- users/userRouter.js
+++ users/userRouter.js
@@ -20,11 +20,60 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/:id', (req, res) => {});
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  db.getById(id)
+    .then(user => {
+      if (!user) {
+        res
+          .status(400)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        res.status(200).json({ user });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The user information could not be retrieved.' });
+    });
+});
 
-router.get('/:id/posts', (req, res) => {});
+router.get('/:id/posts', (req, res) => {
+  const id = req.params.id;
+  db.getUserPosts(id)
+    .then(post => {
+      if (!post) {
+        res
+          .status(400)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        res.status(200).json({ post });
+      }
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: 'The post information could not be retrieved.' });
+    });
+});
 
-router.delete('/:id', (req, res) => {});
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  db.remove(id)
+    .then(deleted => {
+      if (!deleted) {
+        res
+          .status(404)
+          .json({ message: 'The post with the specified ID does not exist.' });
+      } else {
+        res.status(200).json({ deleted });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'The post could not be removed' });
+    });
+});
 
 router.put('/:id', (req, res) => {});
 
